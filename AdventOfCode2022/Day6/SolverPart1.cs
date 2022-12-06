e7c7befcf0289a56142d3348025f46df4a906d29
MostafaBleu <MostafaBleu@users.noreply.github.com>
2022-12-06 08:59:13 -0500

    One Line

--- AdventOfCode2022/Day6/SolverPart1.cs
+++ AdventOfCode2022/Day6/SolverPart1.cs
@@ -5,11 +5,6 @@ public class SolverPart1
     public int Execute(string[] inputs)
     {
         var input = inputs[0];
-        for (var i = 0; i < input.Length - 4; i++)
-        {
-            if (input.Substring(i, 4).Distinct().Count() == 4)
-                return i +4;
-        }
-        return -1;
+        return Enumerable.Range(0, input.Length).First(i => input.Substring(i, 4).Distinct().Count() == 4) + 4;
     }
 }
--- AdventOfCode2022/Day6/SolverPart2.cs
+++ AdventOfCode2022/Day6/SolverPart2.cs
@@ -5,11 +5,6 @@ public class SolverPart2
     public int Execute(string[] inputs)
     {
         var input = inputs[0];
-        for (var i = 0; i < input.Length - 14; i++)
-        {
-            if (input.Substring(i, 14).Distinct().Count() == 14)
-                return i + 14;
-        }
-        return -1;
+        return Enumerable.Range(0, input.Length).First(i => input.Substring(i, 14).Distinct().Count() == 14) + 14;
     }
 }
